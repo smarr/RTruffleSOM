baa26d1618c08a24a10dcf7278897c54dca211a1
Stefan Marr <git@stefan-marr.de>
2014-04-02 14:07:22 +0200

    Make source section a mandatory argument for SOMNode and ExpressionNode
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/nodes/expression_node.py
+++ src/som/interpreter/nodes/expression_node.py
@@ -3,7 +3,7 @@ from som.interpreter.nodes.som_node import SOMNode
 
 class ExpressionNode(SOMNode):
 
-    def __init__(self, executes_enforced, source_section = None):
+    def __init__(self, executes_enforced, source_section):
         SOMNode.__init__(self, executes_enforced, source_section)
 
     def is_super_node(self):
--- src/som/interpreter/nodes/som_node.py
+++ src/som/interpreter/nodes/som_node.py
@@ -3,7 +3,7 @@ from rtruffle.node import Node
 
 class SOMNode(Node):
 
-    def __init__(self, enforced, source_section = None):
+    def __init__(self, enforced, source_section):
         Node.__init__(self, source_section)
         self._executes_enforced = enforced
 
