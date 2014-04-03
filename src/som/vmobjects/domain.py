de199142c14b667f303fec7f4fa54be3bc596e0d
Stefan Marr <git@stefan-marr.de>
2014-04-03 22:11:29 +0200

    Fixed primitive execution and added reading globals and fields to domain
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/vmobjects/domain.py
+++ src/som/vmobjects/domain.py
@@ -8,5 +8,22 @@ def get_domain_for_new_objects(domain):
 
 def request_primitive_execution(domain, prim, rcvr, args):
     universe = prim.get_universe()
-    return domain.send_unenforced("requestExectionOfPrimitive:with:on:",
-                                  [prim, args, rcvr], universe, domain)
+    if args is None:
+        som_args = universe.nilObject
+    else:
+        som_args = universe.new_array_from_list(args, domain)
+    return domain.send_unenforced("requestExecutionOfPrimitive:with:on:",
+                                  [prim, som_args, rcvr], universe, domain)
+
+
+def read_global(domain, symbol, universe):
+    return domain.send_unenforced("readGlobal:", [symbol], universe, domain)
+
+
+def read_field_of(domain, field_idx, obj, universe):
+    assert isinstance(obj, Object)
+    return domain.send_unenforced("readField:of:",
+                                  ## REM: need to pass on idx + 1 because on the
+                                  ##      SOM site, we have 1-based offsets
+                                  [universe.new_integer(field_idx + 1), obj],
+                                  universe, domain)
