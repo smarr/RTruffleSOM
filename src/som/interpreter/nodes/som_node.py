7a0b826bc1597920295d6c07c5309a0622f49b9d
Stefan Marr <git@stefan-marr.de>
2014-04-08 00:43:21 +0200

    _executes_enforced is a constant
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/nodes/som_node.py
+++ src/som/interpreter/nodes/som_node.py
@@ -3,6 +3,8 @@ from rtruffle.node import Node
 
 class SOMNode(Node):
 
+    _immutable_fields_ = ['_executes_enforced']
+
     def __init__(self, enforced, source_section):
         Node.__init__(self, source_section)
         self._executes_enforced = enforced
