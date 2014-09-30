0d90e6426176e8d5f6c5117dfa25874eb40f2aee
Stefan Marr <git@stefan-marr.de>
2014-09-30 14:56:08 +0200

    Remove CachedGlobalReadNode
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/interpreter/nodes/global_read_node.py
+++ src/som/interpreter/nodes/global_read_node.py
@@ -13,47 +13,8 @@ class UninitializedGlobalReadNode(ExpressionNode):
 
     def execute(self, frame):
         if self._universe.has_global(self._global_name):
-            return self._specialize().execute(frame)
+            assoc = self._universe.get_globals_association(self._global_name)
+            return assoc.get_value()
         else:
             return frame.get_self().send_unknown_global(self._global_name,
                                                         self._universe)
-
-    def _specialize(self):
-        glob = self._global_name.get_string()
-        if glob == "true":
-            cached = ConstantGlobalReadNode(trueObject,
-                                            self.get_source_section())
-        elif glob == "false":
-            cached = ConstantGlobalReadNode(falseObject,
-                                            self.get_source_section())
-        elif glob == "nil":
-            cached = ConstantGlobalReadNode(nilObject,
-                                            self.get_source_section())
-        else:
-            assoc = self._universe.get_globals_association(self._global_name)
-            cached = CachedGlobalReadNode(assoc, self.get_source_section())
-        return self.replace(cached)
-
-
-class CachedGlobalReadNode(ExpressionNode):
-
-    _immutable_fields_ = ['_assoc']
-
-    def __init__(self, assoc, source_section):
-        ExpressionNode.__init__(self, source_section)
-        self._assoc = assoc
-
-    def execute(self, frame):
-        return self._assoc.get_value()
-
-
-class ConstantGlobalReadNode(ExpressionNode):
-
-    _immutable_fields_ = ['_value']
-
-    def __init__(self, value, source_section):
-        ExpressionNode.__init__(self, source_section)
-        self._value = value
-
-    def execute(self, frame):
-        return self._value
